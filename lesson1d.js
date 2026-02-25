7df4f6681bd613bb3f815dc0a28eba492c9f065d
Your Name <you@example.com>
2026-02-25 18:38:18 +0300

    updates

--- lesson1d.js
+++ lesson1d.js
@@ -28,3 +28,68 @@ let username = null
 console.log("What is the user name:", null)
 
 // Assignment research and come up with examples on objects and Array data types and show how to access items of the object and of an array
+// Object data type - Used to store  multiple values in a single variable as key-value pair.
+let person = {
+    name : "Caleb",
+    age : 18,
+    isDeveloper : true
+
+};
+console.log(person)
+
+// How to access items in object data type
+//Dot notation
+console.log(person.name);  // Caleb
+// Bracket Notation
+console.log(person["age"]); // age
+
+//Objects can store different data types
+let user = {
+  username: "tonny",
+  score: 100,
+  isOnline: true,
+  skills: ["HTML", "CSS", "JavaScript"], // Array inside object
+  greet: function() {
+    console.log("Hello!");
+  }
+};
+//Strings,Numbers,Booleans, Arrays,Functions(methods),Even other objects.
+
+
+// Arrays - special type of object used to store multiple values in order(THINK OF A LIST)
+let fruits = ["Apple", "Mango", "Orange"];
+
+// Accessing
+console.log(fruits)
+console.log(fruits[0]); //Apple
+console.log(fruits[1]);  // Mango
+
+// Arrays store different types of data types
+let mixed = ["Caleb", 22, true, ["HTML", "CSS"]];
+//Strings,Numbers,Booleans,Other arrays, Objects
+
+// CHanging arrays
+let numbers = [10, 20, 30];
+
+numbers[1] = 50;
+
+console.log(numbers); // [10, 50, 30]
+
+// Add to the end
+fruits.push("Banana");
+// Remove last item
+fruits.pop();
+// Add at the beggining
+fruits.unshift("Pineapple");
+// remove first item
+fruits.shift();
+
+
+// | Array              | Object              |
+// | ------------------ | ------------------- |
+// | Ordered list       | Key-value pairs     |
+// | Uses index numbers | Uses named keys     |
+// | `["A", "B"]`       | `{ name: "Caleb" }` |
+
+
+
